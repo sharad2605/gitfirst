38ae3044e7d302064dde7dbd485995f1778a23f4
sharad2605 <sharad26shukla@gmail.com>
2024-02-17 15:48:31 +0530

    getelementbyid page

--- home.js
+++ home.js
@@ -1,1 +1,15 @@
-console.log(" I love coding");
+// console.log(" I love coding");
+
+const header= document.getElementById('header');
+header.style.backgroundColor='green';
+header.style.borderBottom='2px solid orange';
+
+
+const heading = document.getElementById('main-heading');
+heading.textContent='Fruit World';
+
+const miniheading = document.getElementById('basket-heading');
+miniheading.style.color='green';
+
+const para =document.getElementById('thanks')
+para.innerHTML='<p>Please visit us again</p>'
